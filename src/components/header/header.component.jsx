13ebf82d5886bca8845eee3b012c0f2e7b3f32d3
Franklin Liu <franklin.liu@autodata.net>
2019-07-26 12:38:50 -0400

    cart toggling added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className="header">
         <Link className = 'logo-container' to ="/">
             <Logo className = 'logo'/>
@@ -25,12 +25,15 @@ const Header = ({currentUser}) => (
             }
             <CartIcon/>
         </div>
-        <CartDropdown/>
+       {
+           hidden ? null :  <CartDropdown/>
+       }
     </div>
 );
 //this nameing can be anything but mapStateToProps is standard with redux codebases
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
